70edd02ce1ddac53d22fe0a9715ca499c0f165d6
sein <tpdls973@naver.com>
2021-11-23 22:54:45 +0900

    임시커밋

--- main.js
+++ main.js
@@ -7,23 +7,56 @@ var qna = require('./view/qna/qna')
 var app = http.createServer(function(request,response){
 
     var url = request.url;
-    if(request.url === '/' || request.url === '/main'){
-        url = '/view/mainPage.html';
-        response.writeHead(200);
-        response.end(fs.readFileSync(__dirname + url));
-    }else if(request.url === '/main/qna/detail') {
-        response.writeHead(200);
-        response.end(bar.HTML() + qna.question());
-    }else if(request.url === '/main/qna/create') {
-        response.writeHead(200);
-        response.end(bar.HTML() + qna.question_creat());
-    }else if(request.url === '/main/myPage') {
-        url = '/view/mypage/myPage.html';
-        response.end(fs.readFileSync(__dirname + url));
-    }else {
-        response.writeHead(400);
-        response.end(fs.readFileSync(__dirname + url));
+
+    if(request.url == '/favicon.ico'){
+        return response.writeHead(404);
     }
 
+    if(request.url === '/' || request.url === '/main'){
+        url = '/view/mainPage.html';
+    // }else if(request.url === '/main/qna/detail') {
+    //     qna.read(request,response)
+    // }else if(request.url === '/main/qna/create') {
+    //     qna.creat(request,response)
+    // }else if(request.url === '/main/qna/create_process') {
+    //     qna.create_process(request,response)
+    // }else if(request.url === '/main/qna/update_process') {
+    //     qna.update_process(request,response)
+    // }else if(request.url === '/main/qna/delete_process') {
+    //     qna.delete_process(request, response)
+    // }else if(request.url === '/main/myPage') {
+    //     url = '/view/mypage/myPage.html';
+    //     response.end(fs.readFileSync(__dirname + url));
+    }
+
+    response.writeHead(200);
+    response.end(fs.readFileSync(__dirname + url));
+    // response.writeHead(400);
+    // response.end('Not found');
+
+
+
+    // var url = request.url;
+    // if(request.url === '/'){
+    //     url = '/view/mainPage.html';
+    // }
+    // if(request.url == '/favicon.ico'){
+    //     return response.writeHead(404);
+    // }
+    // if(request.url === '/'){
+    //     url = '/view/mainPage.html';
+    // }
+    // if(request.url === '/main/qna/detail') {
+    //     url = '/view/qna/question.html';
+    // }
+    // if(request.url === '/main/qna/create') {
+    //     url = '/view/qna/questionCreate.html';
+    // }
+    // if(request.url === '/main/myPage') {
+    //     url = '/view/mypage/myPage.html';
+    // }
+    // response.writeHead(200);
+    // response.end(fs.readFileSync(__dirname + url));
+
 });
-app.listen(3001);
+app.listen(4005);
--- view/qna/qna.js
+++ view/qna/qna.js
@@ -1,14 +1,94 @@
-const fs = require('fs');
+const db = require('../../lib/db');
+const template = require('./template.js');
+const qs = require("querystring");
+const url = require("url");
+const sanitizeHtml = require('sanitize-html');
 
-module.exports = {
-    question : function (){
-        let url = '/question.html';
-        return fs.readFileSync(__dirname + url);
-    },
-    question_creat : function (){
-        let url = '/questionCreate.html';
-        return fs.readFileSync(__dirname + url);
-    },
+exports.read = function(request, response){
+    var _url = request.url;
+    var queryData = url.parse(_url, true).query;
+    db.query(`SELECT * FROM question WHERE board_id=? AND id=?`,[queryData.id, queryData.board_id], function(error, question){
+        if(error){
+            throw error;
+        }
+        db.query(`SELECT count(*) FROM scrap WHERE board_id=? AND id=?`,[queryData.id, queryData.board_id], function(error2, scrap){
+            if(error2){
+                throw error2;
+            }
+            let data = {
+                ...question,
+                scrap
+            }
+            let html = template.question_read(data);
+            response.writeHead(200);
+            response.end(html);
+        })
+    })
+}
+
+exports.creat = function (request, response){
+    let html = template.question_creat()
+    response.writeHead(200)
+    response.end(html);
+}
+
+exports.create_process = function(request, response){
+    let body = '';
+    request.on('data', function(data){
+        body = body + data;
+    });
+    request.on('end', function(){
+        var post = qs.parse(body);
+        db.query(`
+            INSERT INTO question (board_id, first_date, last_date, user_id, title, content) 
+              VALUES(?, NOW(), NOW(), ?, ?, ?)`,
+            [post.board_id, post.user_id, post.title, post.content],
+            function(error, result){
+                if(error){
+                    throw error;
+                }
+                response.writeHead(302, {Location: `/?id=${result.insertId}`});
+                response.end();
+            }
+        )
+    });
+}
+
+exports.update = function (request, response){
+    let html = template.question_creat()
+
+    response.writeHead(200)
+    response.end(html);
+}
 
 
+exports.update_process = function(request, response){
+    var body = '';
+    request.on('data', function(data){
+        body = body + data;
+    });
+    request.on('end', function(){
+        var query = qs.parse(body);
+        db.query('UPDATE question SET title=?, content=?, last_date=? WHERE board_id = ? AND no = ?', [query.title, query.content, query.board_id, query.no], function(error, result){
+            response.writeHead(302, {Location: `/?id=${query.id}`});
+            response.end();
+        })
+    });
+}
+
+exports.delete_process = function(request, response){
+    var body = '';
+    request.on('data', function(data){
+        body = body + data;
+    });
+    request.on('end', function(){
+        let query = qs.parse(body);
+        db.query('DELETE FROM question WHERE board_id = ? AND no = ?', [query.board_id, query.no], function(error, result){
+            if(error){
+                throw error;
+            }
+            response.writeHead(302, {Location: `/`});
+            response.end();
+        });
+    });
 }
