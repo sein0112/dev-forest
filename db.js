e7299af56e090c0fb4e09b19d4c2ac315e289fdd
sein <tpdls973@naver.com>
2021-12-17 23:06:28 +0900

    db.js module.exports 추가

--- db.js
+++ db.js
@@ -1,15 +1,17 @@
 var mysql      = require('mysql');
-var connection = mysql.createConnection({
+var db = mysql.createConnection({
     host     : '34.64.141.121',    // 호스트 주소
     user     : 'devF',           // mysql user
     password : 'roo!',       // mysql password
     database : 'devforest'         // mysql 데이터베이스
 });
-connection.connect();
+db.connect();
 // 동작 확인용
-connection.query('SELECT * from gradetbl',
+db.query('SELECT * from gradetbl',
     function (error, results, fields) {
         if (error) throw error;
         console.log('The gradetbl is >>>>>>>>>>>>>>>>>> : ', results);
     });
-connection.end();
+// db.end();
+
+module.exports = db;
