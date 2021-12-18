28306c0a5d2051ad6cad2670ffabaf82f4234fab
준서\kjmj1 <kjmj13@naver.com>
2021-12-18 11:51:15 +0900

    로그인 세션

--- main.js
+++ main.js
@@ -6,8 +6,26 @@ var myPage = require('./view/mypage/myPage.js')
 var loginRoutes = require('./view/login/loginRouter.js');
 var express = require('express');
 var app = express();
+const session = require('express-session');
+var MySQLStore = require('express-mysql-session')(session);
+
 app.use(express.static(__dirname + '/asset'));
 app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(session({
+    key: "user",
+    secret: "user",
+    resave: false,
+    saveUninitialized: false,
+    store: new MySQLStore({
+        host:   'localhost',
+        port:   3306,
+        user:   'root',
+        password:'root',
+        database: 'devforest'
+    })
+}));
+
 
 app.get('/', function(request, response){
     response.sendFile(__dirname + '/view/mainPage.html');
--- view/login/loginRouter.js
+++ view/login/loginRouter.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const app = express();
 const router  = express.Router();
 const template = require('./loginTemplate.js');
 const db =  require('../../db.js');  
+const session = require('express-session');
+const res = require('express/lib/response');
+var MySQLStore = require('express-mysql-session')(session);
 
 router.get('/', function(request, response) {
     response.send(template.container());
@@ -15,6 +19,10 @@ router.post('/loginprocess', function(request, response){
         if (error) response.send("alert('로그인 실패');");
         else {
             console.log('로그인 성공');
+
+            request.session.authenticate = true;
+            console.log(request.session);
+            response.redirect('/myPage');
         }
     });
 });
