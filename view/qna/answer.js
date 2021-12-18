a37118cd234a8b2ed5c09df7355ea2dc4272171b
YNKa526 <rkdbsk0526@gmail.com>
2021-12-18 19:37:51 +0900

    답변 출력하기 기능 추가

--- view/qna/answer.js
+++ view/qna/answer.js
@@ -1,19 +1,22 @@
-var express = require('express'); 
-var router = express.Router();
-var url = require('url');
 const template = require('./ansTemplate.js');
+const db = require('./../../db.js');
 
-var db = require('./db.js');
-
-router.get('/answer', function(request, response){
-    
-});
-
-// exports.container = function(request, response) {
-//     var _url = request.url;
-//     var queryData = url.parse(_url, true).query;
-
-//     var html = template.container();
-
-//     response.send(html);
-// }
+exports.container = function(request, response) {
+    // let userid='abc@naver.com';
+        let userid = request.session.userid;
+        console.log(userid)
+        if (!request.session.userid) {
+            return response.redirect('/');
+        }
+        let html;
+        //게시판ID와 게시글NO의 값을 받아와야함. 일단 임시로 1,1로 지정함.
+        let boardId = 1, qusetNo = 1;
+        db.query('SELECT * FROM usertbl JOIN answerstbl ON usertbl.id = answerstbl.user_id WHERE answerstbl.board_id=? AND answerstbl.quest_no=?', [boardId, qusetNo], function(error, answer) {
+            if(error) console.log(error);
+            else{
+                console.log(answer);
+                html = template.HTML(answer);
+            }
+            response.send(html);
+        });
+}
