ccb84f7d93154dc1df22fba3dfcf1463c02ff7c7
jieun531 <kje053199@naver.com>
2021-12-19 03:55:51 +0900

    development 파일 내용 추가 + showBulletineTemplate 변경

--- view/bulletine/showBulletineTemplate.js
+++ view/bulletine/showBulletineTemplate.js
@@ -141,121 +141,140 @@ module.exports = {
         <!DOCTYPE html>
         <head>
             <meta charset="utf-8">
-            <title>마이페이지</title>
-            <link rel="stylesheet" type="text/css" href="/css/mypage/myPage.css">
-            <link rel="stylesheet" type="text/css" href="/css/nav.css">
+            <title>마이페이지 - 내가 좋아요 / 즐겨찾기 한 글</title>
+            <link rel="stylesheet" href="../../asset/css/mypage/myPage.css">
+            <link rel="stylesheet" href="../../asset/css/nav.css">
+            <link rel="stylesheet" href="../../asset/css/mypage/myPageDetails.css">
+
         </head>
         <body>
             <div id="wrap">
                 <div id="nav">
                     <div id="menu">
-                        <img id="menuicon" src="/image/hamburgerbar2.png" alt="menu" onClick="menuClick()" style="cursor: pointer;">
-                        <img id="menuiconX" src="/image/X.png" onClick="menuClick()" style="visibility: hidden; cursor: pointer;">
+                        <img id="menuicon" src="../../asset/image/hamburgerbar2.png" alt="menu" onClick="menuClick()" style="cursor: pointer;">
+                        <img id="menuiconX" src="../../asset/image/X.png" onClick="menuClick()" style="visibility: hidden; cursor: pointer;">
                         <div id="menuList" style="visibility: hidden;">
                             <ul id="hamburger_menu">
-                                <div id="toHome" style="cursor:pointer;" onClick="location.href='../mainPage.html'">
-                                    <img class="menu_icon" src="/image/home.png">
-                                    <li class="bigMenu"><a href="../..">Home</a></li>
+                            <div id="toHome" style="cursor:pointer;" onClick="location.href='../mainPage.html'">
+                                <img class="menu_icon" src="../../asset/image/home.png">
+                                <li class="bigMenu"><a href="../..">Home</a></li>
+                            </div>
+                            <div id="recentlyAsked">
+                                <img class="menu_icon" src="../../asset/image/chat.png">
+                                <li class="bigMenu"><a href="../..">최근 질문 보기</a></li>
+                            </div>
+                            <div id="postQuest">
+                                <img class="menu_icon" src="../../asset/image/pencil.png">
+                                <li class="bigMenu"><a href="../..">질문 작성하기</a></li>
+                            </div>
+                            <div id="posts">
+                                <img class="menu_icon" src="../../asset/image/tag.png">
+                                <li class="bigMenu"><a href="../..">게시판</a></li>
+                                <div id="tags">
+                                    <a href="../.." class="left">javasript</a>
+                                    <a href="../.." class="right">ruby</a>
+                                    <a href="../.." class="left">python</a>
+                                    <a href="../.." class="right">swift</a>
+                                    <a href="../.." class="left">java</a>
+                                    <a href="../.." class="right">arrays</a>
+                                    <a href="../.." class="left">php</a>
+                                    <a href="../.." class="right">c</a>
+                                    <a href="../.." class="left">android</a>
+                                    <a href="../.." class="right">asp.net</a>
+                                    <a href="../.." class="left">html</a>
+                                    <a href="../.." class="right">.net</a>
+                                    <a href="../.." class="left">jquery</a>
+                                    <a href="../.." class="right">sql</a>
+                                    <a href="../.." class="left">css</a>
+                                    <a href="../.." class="right">r</a>
+                                    <a href="../.." class="left">mysql</a>
+                                    <a href="../.." class="right">sql-server</a>
+                                    <a href="../.." class="left">c++</a>
+                                    <a href="../.." class="right">swift</a>
+                                    <a href="../.." class="left">reactjs</a>
+                                    <a href="../.." class="right">ajax</a>
+                                    <a href="../.." class="left">ios</a>
+                                    <a href="../.." class="right">pandas</a>
+                                    <a href="../.." class="left">node.js</a>
+                                    <a href="../.." class="right">linux</a>
+                                    <a href="../.." class="left">django</a>
+                                    <a href="../.." class="right">angularjs</a>
                                 </div>
-                                <div id="recentlyAsked">
-                                    <img class="menu_icon" src="/image/chat.png">
-                                    <li class="bigMenu"><a href="../..">최근 질문 보기</a></li>
-                                </div>
-                                <div id="postQuest">
-                                    <img class="menu_icon" src="/image/pencil.png">
-                                    <li class="bigMenu"><a href="../..">질문 작성하기</a></li>
-                                </div>
-                                <div id="posts">
-                                    <img class="menu_icon" src="/image/tag.png">
-                                    <li class="bigMenu"><a href="../..">게시판</a></li>
-                                    <div id="tags">
-                                        <a href="/board/1" class="left">javasript</a>
-                                        <a href="/board/2" class="right">ruby</a>
-                                        <a href="/board/3" class="left">python</a>
-                                        <a href="/board/4" class="right">swift</a>
-                                        <a href="/board/5" class="left">java</a>
-                                        <a href="/board/6" class="right">arrays</a>
-                                        <a href="/board/7" class="left">php</a>
-                                        <a href="/board/8" class="right">c</a>
-                                        <a href="/board/9" class="left">android</a>
-                                        <a href="/board/10" class="right">asp.net</a>
-                                        <a href="/board/11" class="left">html</a>
-                                        <a href="/board/12" class="right">.net</a>
-                                        <a href="/board/13" class="left">jquery</a>
-                                        <a href="/board/14" class="right">sql</a>
-                                        <a href="/board/15" class="left">css</a>
-                                        <a href="/board/16" class="right">r</a>
-                                        <a href="/board/17" class="left">mysql</a>
-                                        <a href="/board/18" class="right">sql-server</a>
-                                        <a href="/board/19" class="left">c++</a>
-                                        <a href="/board/20" class="right">swift</a>
-                                        <a href="/board/21" class="left">reactjs</a>
-                                        <a href="/board/22" class="right">ajax</a>
-                                        <a href="/board/23" class="left">ios</a>
-                                        <a href="/board/24" class="right">pandas</a>
-                                        <a href="/board/25" class="left">node.js</a>
-                                        <a href="/board/26" class="right">linux</a>
-                                        <a href="/board/27" class="left">django</a>
-                                        <a href="/board/28" class="right">angularjs</a>
-                                    </div>
-                                </div>
-                            </ul>                        
-                        </div>
-                    </div>
-                    <div id="search">
-                        <form action="/">
-                            <img id="search_icon" src="/image/searchicon.png" alt="search">
-                            <input id="search_query" type="text">
-                            <button id="search_btn" style="cursor:pointer;">search</button>
-                        </form>
-                    </div>
-                    ${navhtml}
-                </div>
-                <div id="contents">
-                    <div id="userInfo">
-                        <img id="usrProfImgBig" src="내사진.jpg" alt="userpng">
-                        <table id="userInfoTable">
-                        ${userinfohtml}
-                        </table>
-                        <button id="userInfoEdit_btn" style="cursor: pointer;" onClick="location.href='/modify'">편집</button>
-                    </div>
-                    <hr style="width: 80%; margin: 0 auto; margin-bottom: 20px; border: solid 1px #A1A1A1">
-                    <div id="userPost">
-                        <p class="userPost_label">최근 내가 질문 한 글</p>
-                        <div id="myQuest">
-                            ${questionshtml}
-                            <!--<img src="점점점.png" style="width:150px; height: 50px; float:left; margin-top: 115px;">-->
-                        </div>
-                        <p class="userPost_label" style="cursor: pointer;">최근 내가 답변 한 글</p>
-                        <div id="myAnsw">
-                            ${myanswerquestshtml}
-                        </div>
-                        <p class="userPost_label" style="cursor: pointer;">즐겨찾기한 글</p>
-                        <div id="myScrap">
-                            ${myscraphtml}
-                        </div>
-                        <p class="userPost_label" style="cursor: pointer;">좋아요한 답변</p>
-                        <div id="myLike">
-                            ${mylikehtml}
-                        </div>
+                            </div>
+                        </ul>                        
                     </div>
                 </div>
+                <div id="search">
+                    <form action="/">
+                        <img id="search_icon" src="../../asset/image/searchicon.png" alt="search">
+                        <input id="search_query" type="text">
+                        <button id="search_btn" style="cursor:pointer;">search</button>
+                    </form>
+                </div>
+                ${navhtml}
+                <div id="userInfo">
+                <img id="usrProfImgBig" src="내사진.jpg" alt="userpng">
+                <table id="userInfoTable">
+                ${userinfohtml}
+                </table>
+                <button id="userInfoEdit_btn" style="cursor: pointer;" onClick="location.href='/modify'">편집</button>
             </div>
-        </body>
-        <script>
-            function menuClick(){
-                var toggle = document.getElementById('menuList').style.visibility;
-                if (toggle === 'hidden'){
-                    document.getElementById('menuList').style.visibility = 'visible';
-                    document.getElementById('menuiconX').style.visibility = 'visible';
-                }
-                else{
-                    document.getElementById('menuList').style.visibility = 'hidden';
-                    document.getElementById('menuiconX').style.visibility = 'hidden';
-                }
+            </div>
+
+            <div class="bulletin">
+                <img src="../../asset/image/homeWithNoBack.png" width="35px" height="35px" style="float: left; margin-top: 5px;">
+                <p style="margin-top:12px; margin-left: 10px; float: left; text-align: left;">
+                    <a href="myPage.html", style="color: black;">마이페이지</a> - <span>내가 질문한 글 / 내가 답변한 글</span></p>
+            </div>
+
+
+            <div class="box1-group", style="margin-top: 10px;">
+                <span class ="box1", style="margin-left: 0px;">
+                    ${myscraphtml}
+                </span>
+        
+                <span class ="box1", style="margin-left: 15px; margin-right: 15px;">
+                    ${myscraphtml}
+                </span>
+                
+                <span class ="box1", style="margin-left: 0px; margin-right: 0px;">
+                    ${myscraphtml}
+                </span>
+            </div>
+        
+            <div class="box1-group", style="margin-top: 20px;">
+                <span class ="box1", style="margin-left: 0px;">
+                    ${myscraphtml}
+                </span>
+        
+                <span class ="box1", style="margin-left: 15px; margin-right: 15px;">
+                    ${myscraphtml}
+                </span>
+                
+                <span class ="box1", style="margin-left: 0px; margin-right: 0px;">
+                    ${myscraphtml}
+                </span>
+            </div>
+        
+            <button style="width:200px; height:55px; margin-left: 690px; margin-right: auto; margin-top: 40px; background-color: #e5e5e5; border: 0px;">
+                <img src="../../asset/image/more.png" style="position:absolute; width: 100px; height: auto; margin-left: -50px; margin-top: -15px;">
+        </div>
+        </div>
+    </body>
+    <script>
+        function menuClick(){
+            var toggle = document.getElementById('menuList').style.visibility;
+            if (toggle === 'hidden'){
+                document.getElementById('menuList').style.visibility = 'visible';
+                document.getElementById('menuiconX').style.visibility = 'visible';
             }
-        </script>
-    </html>
+            else{
+                document.getElementById('menuList').style.visibility = 'hidden';
+                document.getElementById('menuiconX').style.visibility = 'hidden';
+            }
+        }
+    </script>
+</html>
         `;
     }
 }
