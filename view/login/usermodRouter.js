b4cb03add072604c956e784c6ad4333f94e9f485
준서\kjmj1 <kjmj13@naver.com>
2021-12-18 18:32:38 +0900

    코멘트 삭제

--- view/login/usermodRouter.js
+++ view/login/usermodRouter.js
@@ -4,7 +4,7 @@ const template = require('./usermodTemplate.js');
 const db =  require('../../db.js');  
 
 router.get('/', function(request, response) {
-    console.log(request.session);
+    // console.log(request.session);
     if ( request.session.authenticate == undefined || request.session.authenticate == false) { 
         return response.redirect('/login'); 
     };
@@ -12,7 +12,6 @@ router.get('/', function(request, response) {
     db.query(`SELECT nickname, belong FROM usertbl WHERE id=?`, [userid], function(error, userinfo) {
         let user_nickname = userinfo[0].nickname;
         let user_belong = userinfo[0].belong;
-        console.log(user_nickname, user_belong);
         return response.send(template.container(userid, user_nickname, user_belong));
     });
 });
@@ -29,7 +28,6 @@ router.post('/modprocess', function(request, response){
         return response.write("<script>window.location='/modify'</script>");
     }
     db.query(`SELECT nickname, password FROM usertbl where id = ?`, [userid], function(error, userinfo) {
-        console.log(userinfo[0]);
         var userinfo = userinfo[0];
         if (currentpwd !== userinfo.password) {    //비밀번호 입력 잘못 했을 때
             response.write("<script>alert('not correct password')</script>");
@@ -42,7 +40,6 @@ router.post('/modprocess', function(request, response){
                     return response.write("<script>window.location='/modify'</script>");
                 }
                 //수정 성공
-                console.log(result);
                 response.write("<script>alert('success')</script>");                
                 return response.write("<script>window.location='/myPage'</script>");
             });
