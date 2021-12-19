bcf6e916efef51f2816bba0c54232811cf396793
준서\kjmj1 <kjmj13@naver.com>
2021-12-19 11:25:02 +0900

    게시판 (사용자정보제외)

--- view/board/boardRouter.js
+++ view/board/boardRouter.js
@@ -4,9 +4,15 @@ const template = require('./boardTemplate.js');
 const db =  require('../../db.js');  
 
 router.get('/board/:boardId', function(request, response) {
-    response.send(template.container());
-
+    // response.send(template.container());
+    let boardId = request.params.boardId;
+    let posttohtml;
     console.log(request.params.boardId);
+    db.query('SELECT * FROM questionstbl WHERE board_id = ? LIMIT 10', [boardId], function(error, questions) {
+        console.log(questions);
+        posttohtml = template.posts(questions);
+        response.send(template.container(posttohtml));
+    });
 });
 
 
