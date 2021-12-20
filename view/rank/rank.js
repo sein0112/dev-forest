ef9448a73a4f8bfadc94779b9e05fc5c247ab765
준서\kjmj1 <kjmj13@naver.com>
2021-12-20 21:02:32 +0900

    그룹 랭킹 쿼리 수정

--- view/rank/rank.js
+++ view/rank/rank.js
@@ -12,7 +12,7 @@ exports.container = function(request, response) {
     db.query('select user_id, nickname, image, sum(point) as point from answerstbl join usertbl on answerstbl.user_id = usertbl.id group by user_id order by point desc', function(error, userRnak) {
         if(error) console.log(error);
         console.log(userRnak);
-        db.query('SELECT belong, sum(point) FROM usertbl JOIN answerstbl ON usertbl.id=answerstbl.user_id GROUP BY usertbl.belong ORDER BY sum(answerstbl.point)', function(error2, groupRank) {
+        db.query('SELECT belong, sum(point) FROM usertbl JOIN answerstbl ON usertbl.id=answerstbl.user_id GROUP BY usertbl.belong ORDER BY sum(answerstbl.point) desc', function(error2, groupRank) {
             if(error2) console.log(error2);
             else {
                 console.log(groupRank)
