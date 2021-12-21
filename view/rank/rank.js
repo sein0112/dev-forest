f97e9a04b804bbf56bee37a315aba3831968ab3d
준서\kjmj1 <kjmj13@naver.com>
2021-12-21 22:33:06 +0900

    10명 한정

--- view/rank/rank.js
+++ view/rank/rank.js
@@ -8,7 +8,7 @@ exports.container = function(request, response) {
     let userinfo = request.session;
     let navhtml = template.nav(userinfo);
     let html;
-    db.query('select user_id, nickname, image, sum(point) as point from answerstbl join usertbl on answerstbl.user_id = usertbl.id group by user_id order by point desc', function(error, userRnak) {
+    db.query('select user_id, nickname, image, sum(point) as point from answerstbl join usertbl on answerstbl.user_id = usertbl.id group by user_id order by point desc limit 15', function(error, userRnak) {
         if(error) console.log(error);
         db.query('SELECT belong, sum(point) FROM usertbl JOIN answerstbl ON usertbl.id=answerstbl.user_id GROUP BY usertbl.belong ORDER BY sum(answerstbl.point) desc', function(error2, groupRank) {
             if(error2) console.log(error2);
