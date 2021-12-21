75b74330ab97878ccd1358fe9bbd2539712a3c19
준서\kjmj1 <kjmj13@naver.com>
2021-12-21 13:33:42 +0900

    마이페이지 포인트 수정

--- view/mypage/myPage.js
+++ view/mypage/myPage.js
@@ -11,8 +11,15 @@ exports.container = function(request, response) {
     let html;
     let user;
     let userinfohtml, navhtml, questionshtml, myanswerquestshtml, myscraphtml, mylikehtml;
-
-    db.query('SELECT * FROM usertbl JOIN gradetbl ON usertbl.level = gradetbl.level where usertbl.id=?', [userid], function(error, users) {
+    let sql = '\
+        SELECT id, nickname, belong, image, usertbl.level, grade_date, gradetbl.name, sum(point) as point FROM usertbl \
+        JOIN gradetbl \
+        ON usertbl.level = gradetbl.level \
+        join answerstbl\
+        on user_id = id\
+        where usertbl.id=?\
+    ';
+    db.query(sql, [userid], function(error, users) {
         if(error) console.log(error);
         else{
             user = users[0];
